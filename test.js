494bc15fa64bb3f8557be2e3345d05f636161a71
scrunchygamergirl <nataniahwang1010@gmai.com>
2022-08-07 14:19:36 -0400

    test

---
+++ test.js
@@ -0,0 +1,7 @@
+var name = "yor forger";
+let age = 27;
+function saysomething(name, age) {
+    console.log("Your hero has been protecting the earth for x amount of years.");
+
+}
+saysomething(name, age)
